bacccd80dbb085d2c54057f16d495272cb2126fd
Jie Han <jiehan@ieee.org>
2025-03-07 21:12:46 +0000

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const toggleButton = document.getElementById("toggle-sidebar");
-    const sidebar = document.getElementById("sidebar");
-
-    toggleButton.addEventListener("click", function () {
-        sidebar.classList.toggle("active"); // 切换侧边栏的显示/隐藏
-    });
-});
